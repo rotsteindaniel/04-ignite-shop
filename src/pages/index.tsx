67b2b42e9b0ba41a62c896ab26c6b5ded63a23a9
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2022-10-11 17:19:04 -0300

    product-checkout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/future/image";
+import Link from "next/link";
 
 import { useKeenSlider } from "keen-slider/react";
 
@@ -7,13 +9,14 @@ import { stripe } from "../lib/stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import "keen-slider/keen-slider.min.css";
+import Stripe from "stripe";
 
 interface HomeProps {
   products: {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[];
 }
 
@@ -26,19 +29,31 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        );
-      })}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
   );
 }
 
@@ -48,7 +63,7 @@ export const getStaticProps: GetStaticProps = async () => {
   });
 
   const products = response.data.map((product) => {
-    const price = product.default_price as any;
+    const price = product.default_price as Stripe.Price;
 
     return {
       id: product.id,
